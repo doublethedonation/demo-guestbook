204c455da78b567def09cf80749dc223574ea9d7
Jonathon McKitrick <jmckitrick@gmail.com>
2024-04-09 14:10:13 -0400

    whitespace

--- src/cljs/londonclj/guestbook/core.cljs
+++ src/cljs/londonclj/guestbook/core.cljs
@@ -8,74 +8,75 @@
 ;; Views
 
 (defn message-list [messages]
-      [:ul.content
-       (for [{:keys [timestamp message name]} @messages]
-            ^{:key timestamp}
-            [:li
-             [:time (.toLocaleString timestamp)]
-             [:p message]
-             [:p " - " name]])])
+  [:ul.content
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]
+      [:hr]])])
 
 (defn get-messages [messages]
-      (GET "/api/list"
-           {:headers {"Accept" "application/transit+json"}
-            :handler #(reset! messages (vec %))}))
+  (GET "/api/list"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(reset! messages (vec %))}))
 
 (defn send-message! [fields errors messages]
-      (POST "/api/create"
-            {:headers {"Accept"       "application/transit+json"
-                       "x-csrf-token" (.-value (.getElementById js/document "token"))}
-             :params @fields
-             :handler #(do
-                         (reset! errors nil)
-                         (swap! messages conj (assoc @fields :timestamp (js/Date.))))
-             :error-handler #(do
-                               (.log js/console (str %))
-                               (reset! errors (get-in % [:response :errors])))}))
+  (POST "/api/create"
+        {:headers {"Accept"       "application/transit+json"
+                   "x-csrf-token" (.-value (.getElementById js/document "token"))}
+         :params @fields
+         :handler #(do
+                     (reset! errors nil)
+                     (swap! messages conj (assoc @fields :timestamp (js/Date.))))
+         :error-handler #(do
+                           (.log js/console (str %))
+                           (reset! errors (get-in % [:response :errors])))}))
 
 (defn errors-component [errors id]
       (when-let [error (id @errors)]
                 [:div.alert.alert-danger (clojure.string/join error)]))
 
 (defn message-form [messages]
-      (let [fields (r/atom {})
-            errors (r/atom nil)]
-           (fn []
-               [:div.columns>div.column.is-two-thirds>div.content
-                [errors-component errors :server-error]
-                [:div.control
-                 [errors-component errors :name]
-                 [:p "Name:"
-                  [:input.input
-                   {:type      :text
-                    :name      :name
-                    :on-change #(swap! fields assoc :name (-> % .-target .-value))
-                    :value     (:name @fields)}]]
-                 [errors-component errors :message]
-                 [:p "Message:"
-                  [:textarea.textarea
-                   {:rows      4
-                    :cols      50
-                    :name      :message
-                    :value     (:message @fields)
-                    :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
-                 [:input.button
-                  {:type     :submit
-                   :on-click #(send-message! fields errors messages)
-                   :value    "comment"}]]])))
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
+    (fn []
+      [:div.columns>div.column.is-two-thirds>div.content
+       [errors-component errors :server-error]
+       [:div.control
+        [errors-component errors :name]
+        [:p "Name:"
+         [:input.input
+          {:type      :text
+           :name      :name
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value     (:name @fields)}]]
+        [errors-component errors :message]
+        [:p "Message:"
+         [:textarea.textarea
+          {:rows      4
+           :cols      50
+           :name      :message
+           :value     (:message @fields)
+           :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
+        [:input.button
+         {:type     :submit
+          :on-click #(send-message! fields errors messages)
+          :value    "comment"}]]])))
 
 (defn home []
-      (let [messages (r/atom nil)]
-        (get-messages messages)
-           (fn []
-               [:section.section>div.container
-                [:h1.title "Guestbook"]
-                [:div.row
-                 [:div.span12
-                  [message-list messages]]]
-                [:div.row
-                 [:div.span12
-                  [message-form messages]]]])))
+  (let [messages (r/atom nil)]
+    (get-messages messages)
+    (fn []
+      [:section.section>div.container
+       [:h1.title "Guestbook"]
+       [:div.row
+        [:div.span12
+         [message-list messages]]]
+       [:div.row
+        [:div.span12
+         [message-form messages]]]])))
 
 ;; -------------------------
 ;; Initialize app
